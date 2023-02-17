e93c976a6daa0e773e99e943d768680d2923ef07
Igor Paluch <hello.igor@wp.pl>
2023-02-17 15:29:56 +0100

    Restricted key to API hidden

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
 let weather = {
-    apiKey: "3888aa45c015a0abb0d201079e49d789",
-    fetchWeather: function (city) {
-        fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=" +
-            city +
-            "&units=metric&appid=" +
-            this.apiKey
-        )
-            .then((response) => {
-                if (!response.ok) {
-                    alert("No weather found!");
-                    throw new Error("No weather found");
-                }
-                return response.json();
-            })
-            .then((data) => this.displayWeather(data));
-    }, 
-    displayWeather: function (data) {
-        const { name } = data;
-        const { icon, description } = data.weather[0];
-        const { temp, humidity } = data.main;
-        const { speed } = data.wind;
-        document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".temp").innerText = temp + "°C";
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector(".description").innerText = description;
-        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind speed: " + speed + "hm/h";
-        document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
-    }, 
-    search: function () {
-        this.fetchWeather(document.querySelector(".search-bar").value);
-    }
+  apiKey: "*********",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found!");
+          throw new Error("No weather found");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + "hm/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
 };
 
 document.querySelector(".search button").addEventListener("click", function () {
